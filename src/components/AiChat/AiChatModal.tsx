05be9e685ee6f899f4271275659c036efb2b848e
Sohee Kim <ttohee1275@gmail.com>
2025-04-19 09:24:10 +0900

    feat: change modal position

--- src/components/AiChat/AiChatModal.tsx
+++ src/components/AiChat/AiChatModal.tsx
@@ -183,9 +183,8 @@ const ChatContainer = styled.div`
 
   z-index: 100;
   position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  bottom: 2%;
+  right: 2%;
 `;
 
 const TopContainer = styled.div`
